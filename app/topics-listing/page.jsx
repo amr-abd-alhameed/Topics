5e4aec209ffb3ade49b898971269e04c20db8261
amr <aa5473@fayoum.edu.eg>
2023-08-22 15:48:19 +0300

    card

--- app/topics-listing/page.jsx
+++ app/topics-listing/page.jsx
@@ -1,10 +1,12 @@
-"use client";
-import BreadCrumb from "@/components/BreadCrumb";
+"use client";import BreadCrumb from "@/components/BreadCrumb";
 import CardI from "@/components/CardI";
 import React from "react";
 import logo1 from "../../assets/images/topics/undraw_Remote_design_team_re_urdx.png";
+import logo2 from "../../assets/images/topics/colleagues-working-cozy-office-medium-shot.png";
+import logo3 from "../../assets/images/topics/undraw_Redesign_feedback_re_jvm0.png";
 import CardII from "@/components/CardII";
 const numbers = ["1", "2", "3", "4", "5"];
+const img = [logo1, logo2, logo3];
 const TopicsListing = () => {
   return (
     <main>
@@ -14,8 +16,15 @@ const TopicsListing = () => {
           <h3 className="text-center font-semibold text-3xl mb-10">
             Popular Topics
           </h3>
-
-          <CardI
+          {img.map((e, index) => (
+            <CardI
+              key={index}
+              logo={e}
+              active={"active"}
+              spanBG={"bg-red-500"}
+            />
+          ))}
+          {/* <CardI
             key={"one"}
             active="active"
             logo={logo1}
@@ -32,7 +41,7 @@ const TopicsListing = () => {
             active="active"
             logo={logo1}
             spanBG={"bg-red-500"}
-          />
+          /> */}
 
           <div className="flex items-center gap-2 justify-center capitalize text-xl  text-p-color">
             <p className="rounded-[10px] bg-section-bg-color p-3 py-4 px-5">
